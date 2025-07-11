95f687ad28e3eb94a65e2d5279d7be92b228fb38
David <dmonaresss@gmail.com>
2025-07-11 12:01:16 +0300

    Create search component

--- components/header/index.vue
+++ components/header/index.vue
@@ -1,9 +1,20 @@
 <template>
 	<div class="container">
 		<header class="header">
-			
+			<div class="header__content">
+				<div class="header__content-left">
+					<IconsLogo class="header__content-logo" />
+					<UiSearch class="header__content-search" />
+				</div>
+				<div class="header__content-right">
+					<UiLink />
+				</div>
+			</div>
+			<ul class="header__menu">
+				<li></li>
+			</ul>
 		</header>
 	</div>
 </template>
 
-<style lang='scss' scoped></style>
+<style lang="scss" scoped></style>
---
+++ components/ui/search.vue
@@ -0,0 +1,47 @@
+<script setup></script>
+
+<template>
+	<div class="ui-search">
+		<input
+			type="search"
+			class="ui-search__input"
+			placeholder="Автор, название, ISBN"
+		/>
+		<button class="ui-search__button">
+			<IconsSearch class="ui-search__button-icon" />
+			<span class="ui-search__button-text">Искать</span>
+		</button>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.ui-search {
+	@include flex(center);
+	width: 100%;
+	max-width: 570px;
+
+	&__input {
+		width: 100%;
+		padding: 10px 15px;
+		border: 1px solid $border;
+		border-right: none;
+		border-radius: 5px 0 0 5px;
+		outline: none;
+	}
+
+	&__button {
+		padding: 9px 25px;
+		max-height: 40px;
+		background-color: $accent;
+		border: none;
+		border-radius: 0 5px 5px 0;
+		@include flex(center, stretch, 5px);
+		&-icon {
+		}
+
+		&-text {
+			color: $white;
+		}
+	}
+}
+</style>
